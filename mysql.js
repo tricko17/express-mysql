015e8aabeaca8fce39047eeb6f539f75d3d68a48
rioookta <rioookta@gmail.com>
2019-03-31 11:22:43 +0700

    Add Update And Read

--- mysql.js
+++ mysql.js
@@ -20,11 +20,28 @@ db.query(sql, data, (err, result) => {
 console.log("-------------------------------------------------");
 // READ
 sql = "SELECT * FROM karyawan";
-
 db.query(sql, (err, result) => {
     if(err) throw err;
     console.log("Read Data")
     console.log(result);
 });
 
+// DELETE
+let id = 3;
+sql = "DELETE FROM karyawan WHERE No=?";
+db.query(sql,id, (err, result) => {
+    if(err) throw err;
+    console.log("Delete Data");
+    console.log(result);
+});
+
+// Update
+id = 4;
+let nama = "Andi";
+sql = "UPDATE karyawan SET nama=? WHERE No=?";
+db.query(sql,[nama,id], (err, result) => {
+    if(err) throw err;
+    console.log("Update Data");
+    console.log(result);
+});
 db.end();
